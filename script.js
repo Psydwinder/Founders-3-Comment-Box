19da9d8213fc6ec4662630431c34426a2913b8d4
Psydwinder <101563800+Psydwinder@users.noreply.github.com>
2022-07-08 14:09:37 +0100

    Added files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let textArea = document.getElementById("comment");
+let characterCounter = document.getElementById("charCount");
+let warning = document.getElementById("warning");
+const x = 140;
+
+function myFunction() {
+  let name = document.getElementById("name").value;
+  let comment = document.getElementById("comment").value;
+  let email = document.getElementById("email").value;
+
+  let regexEmail = /^\w+([\.-]?\ w+)*@\w+([\.-]?\ w+)*(\.\w{2,3})+$/; // Checks Email
+  if (name && comment && email.match(regexEmail)) {
+    document.getElementById("comment1").innerHTML = comment;
+    document.getElementById("meta_author").innerHTML = name;
+  } else {
+    return alertBox();
+  }
+}
+
+const countCharacters = () => {
+  let numOfEnteredChars = textArea.value.length;
+  let counter = x - numOfEnteredChars;
+  characterCounter.textContent = counter + "/140";
+
+  if (counter < 0) {
+    characterCounter.style.color = "red";
+    warning.innerHTML = "You have entered too many characters.";
+    textArea.style.boxShadow = "none";
+    textArea.style.outline = "1px solid red";
+  } else if (counter < 15) {
+    characterCounter.style.color = "orange";
+    warning.innerHTML = "";
+    textArea.style.boxShadow = "none";
+    textArea.style.outline = "1px solid orange";
+  } else {
+    characterCounter.style.color = "white";
+    warning.innerHTML = "";
+    textArea.style.outline = "none";
+  }
+};
+
+textArea.addEventListener("input", countCharacters);
+
+const submit = document.getElementById("submit");
+submit.addEventListener("click", myFunction);
+// submit returns:
+
+var input = document.getElementById("submit");
+var name2 = document.getElementById("name");
+var email = document.getElementById("email");
+
+function alertBox() {
+  alert("Please Complete Form");
+}
